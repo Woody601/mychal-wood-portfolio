51257310f19980a2231c50a2b3c10cb29d46eb74
Woody601 <mychalwood@gmail.com>
2024-11-22 19:09:14 -0500

    Updated the styling of the hero and the navbar.

--- src/app/page.js
+++ src/app/page.js
@@ -5,18 +5,13 @@ import { Button } from "semantic-ui-react";
 
 export default function Home() {
   return (
-    <>
-      <Head>
-        <title>Home</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <section className="hero home">
+    <div className="home">
+      <section className="hero">
         <h2>Hello There! I'm</h2>
         <h1>Mychal Wood</h1>
         <h2>Gamer, Web Designer, and Front End Developer.</h2>
         <Button href="/about">About Me</Button>
       </section>
-    </>
+    </div>
   );
 }
