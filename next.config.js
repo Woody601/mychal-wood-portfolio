296458c3c6108c0de638436b17a88b4a5c7d7a77
Woody601 <mychalwood@gmail.com>
2026-06-07 19:22:31 -0400

    Fixed some stuff

--- next.config.js
+++ next.config.js
@@ -1,13 +1,10 @@
-import { PHASE_DEVELOPMENT_SERVER } from "next/constants.js";
 /** @type {import('next').NextConfig} */
-const nextConfig = (phase) => ({
-  // Keep production builds from replacing chunks used by the running dev server.
-  distDir: phase === PHASE_DEVELOPMENT_SERVER ? ".next" : ".next-build",
+const nextConfig = {
   sassOptions: {
     loadPaths: [process.cwd()],
     // Next.js injects this string at the top of every SCSS file
     additionalData: `@use "src/app/styles/abstract/_variables.scss" as *;`,
   },
-});
+};
 
 export default nextConfig;
